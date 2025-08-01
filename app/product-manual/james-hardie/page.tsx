98caa23d408351239560035f793fb69adceda16c
KIRII User <user@kirii.com>
2025-08-01 15:57:07 +0800

    Unify download button text to '📄 Download PDF' across all product manual pages

--- app/product-manual/james-hardie/page.tsx
+++ app/product-manual/james-hardie/page.tsx
@@ -68,11 +68,7 @@ export default function JamesHardiePage() {
                     document.body.removeChild(link);
                   }}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height={16} width={16} className="text-white">
-                    <path fill="currentColor" d="M12 2C13.1 2 14 2.9 14 4V12L16.5 9.5C16.9 9.1 17.5 9.1 17.9 9.5C18.3 9.9 18.3 10.5 17.9 10.9L12.7 16.1C12.3 16.5 11.7 16.5 11.3 16.1L6.1 10.9C5.7 10.5 5.7 9.9 6.1 9.5C6.5 9.1 7.1 9.1 7.5 9.5L10 12V4C10 2.9 10.9 2 12 2Z"/>
-                    <path fill="currentColor" d="M20 20H4C3.4 20 3 20.4 3 21C3 21.6 3.4 22 4 22H20C20.6 22 21 21.6 21 21C21 20.4 20.6 20 20 20Z"/>
-                  </svg>
-                  <span>Download {file.name.replace('.pdf', '')} PDF</span>
+                  <span>📄 Download PDF</span>
                 </Button>
               </div>
             </CardContent>
